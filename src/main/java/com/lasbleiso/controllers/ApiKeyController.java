37a9d4cae22c78f6b5547aadd34a8d820e612acf
olivierlasbleis <lasbleis.olivier29@gmail.com>
2020-09-01 18:40:30 +0200

    initialisé

--- src/main/java/com/lasbleiso/controllers/ApiKeyController.java
+++ src/main/java/com/lasbleiso/controllers/ApiKeyController.java
@@ -4,6 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 
+ * controleur lié aux apikey afin qu'un utilisateur ne spam pas les requêtes
+ * @author lasbl
+ *
+ */
 @RestController
 @RequestMapping("station-pollutions")
 public class ApiKeyController {
